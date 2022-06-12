a9f5d6736bf58115fa37b3176078ae5ee7e7bf0d
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-06-12 18:24:45 +0530

    File and Folder Structure Setup Done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import ExerciseDetail from './pages/ExerciseDetail/ExerciseDetail';
+
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/exercise/:id' element={<ExerciseDetail />} />
+			</Routes>
+			<Footer />
+		</Box>
+	);
 }
 
 export default App;
